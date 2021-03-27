153e68f4a0c2eaee93084d429bd5927127dc404c
gokaykucuk <gokay@hey.com>
2021-03-27 01:29:06 +0100

    temp

---
+++ src/tools/downloader.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const {exec} = require('child_process');
+
+export const downloadMacBinaries = async () => {
+    const mac_versions_response = await axios.get("https://formulae.brew.sh/api/formula/git.json")
+    const mac_versions_list = mac_versions_response.data['bottle']['stable']['files']
+    for (const [mac_version, version_data] of Object.entries(mac_versions_list)) {
+        const wget = exec(`wget -O ext/${mac_version} ${version_data['url']}`)
+        wget.on('exit', function() {
+            process.exit()
+        })
+    }
+
+}
+
