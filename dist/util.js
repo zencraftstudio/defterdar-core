73bd620d9a9398030f3fdd71732cba7aa0185dda
gokaykucuk <gokay@hey.com>
2021-03-27 02:56:00 +0100

    deleted zips

---
+++ dist/util.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.consoleLog = void 0;
+const consoleLog = (message) => {
+    console.log(message);
+};
+exports.consoleLog = consoleLog;
