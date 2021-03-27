75eaca33b653dd17f1cf88287fa81ecfe0db19cb
gokaykucuk <gokay@hey.com>
2021-03-27 02:10:17 +0100

    added downloaders

--- src/tools/downloader.test.ts
+++ src/tools/downloader.test.ts
@@ -1,6 +1,11 @@
-import {downloadMacBinaries} from "./downloader";
+import {downloadMacBinaries, downloadWinBinaries} from "./downloader";
 
-test("download mac binaries",async ()=>{
+test("download mac binaries", async () => {
     const downloadMacBinariesResult = await downloadMacBinaries()
     console.log(downloadMacBinariesResult)
 })
+
+test("download windows binaries", async () => {
+    const downloadWindowsBinariesResult = await downloadWinBinaries()
+    console.log(downloadWindowsBinariesResult)
+});
--- src/tools/downloader.ts
+++ src/tools/downloader.ts
@@ -1,16 +1,31 @@
 import axios from "axios";
 
-const {exec} = require('child_process');
+import {execSync} from "child_process"
+import {mkdir, unlink} from "fs/promises";
+
 
 export const downloadMacBinaries = async () => {
     const mac_versions_response = await axios.get("https://formulae.brew.sh/api/formula/git.json")
     const mac_versions_list = mac_versions_response.data['bottle']['stable']['files']
     for (const [mac_version, version_data] of Object.entries(mac_versions_list)) {
-        const wget = exec(`wget -O ext/${mac_version} ${version_data}`)
-        wget.on('exit', function() {
-            process.exit()
-        })
+        const fileName = `ext/${mac_version}.tar.gz`
+        const folderName = `ext/${mac_version}`
+        // @ts-ignore
+        const wgetResult = execSync(`wget -O ${fileName} ${version_data['url']}`)
+        await mkdir(folderName)
+        const unTarResult = execSync(`tar -xf ${fileName} -C ${folderName}`)
+        await unlink(fileName)
     }
 
 }
 
+export const downloadWinBinaries = async () => {
+    const filePrepend = "ext/win";
+    [32, 64].map((arch: number) => {
+        const filePath = `${filePrepend}_${arch}.zip`
+        const wgetResult = execSync(`wget -O ${filePath} https://github.com/git-for-windows/git/releases/download/v2.31.0.windows.1/MinGit-2.31.0-${arch}-bit.zip`)
+        const unzipResult = execSync(`unzip ${filePath} -d ${filePrepend}_${arch}`)
+        unlink(filePath)
+    });
+
+}
